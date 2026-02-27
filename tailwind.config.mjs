156495b91e029461e30a3d3721ce512af271d781
Ankur Jain <ankur.jain@external.cnh.com>
2026-02-27 23:00:10 +0530

    Publishing first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-blue': '#0A2540',
+        'brand-teal': '#00D4FF',
+        'brand-light': '#F6F9FC',
+        'brand-dark': '#0A2540',
+      }
+    },
+  },
+  plugins: [],
+}
